d0687322e5328d6c9fbcfe835b6cdf6064fdcb0d
Adam Chang <lavouver96@gmail.com>
2024-05-18 02:07:04 +0800

    add the blog generator

--- backend/ai_blog_app/ai_blog_app/settings.py
+++ backend/ai_blog_app/ai_blog_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,5 +134,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'
--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -4,8 +4,14 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt 
 from django.http import JsonResponse
+from django.conf import settings
+import os
 import json
 from pytube import YouTube
+import assemblyai as aai
+from openai import OpenAI
+from .models import BlogPost
+
 
 # Create your views here.
 @login_required
@@ -53,23 +59,97 @@ def user_logout(request):
   logout(request)
   return redirect('/')
 
+
+def blog_list(request):
+  blog_articles = BlogPost.objects.filter(user=request.user)
+  return render(request, 'all-blogs.html', {'blog_articles': blog_articles})
+
+
+def blog_details(request, pk):
+  blog_article_detail = BlogPost.objects.get(id=pk)
+  if request.user == blog_article_detail.user:
+    return render(request, "blog-details.html", {"blog_article_detail": blog_article_detail})
+  else:
+    redirect('/')
+
 @csrf_exempt
 def generate_blog(request):
   if request.method == 'POST':
     try:
       data = json.loads(request.body)
       yt_link = data['link']
-      return JsonResponse({'content': yt_link})
     except (KeyError, json.JSONDecodeError):
       return JsonResponse({'error': 'Invalid data sent'}, status=400)
-    
+      
     # get the title
     title = yt_title(yt_link)
 
+    # get the transcipt
+    transcript = get_transcription(yt_link)
+
+    if not transcript:
+      return JsonResponse({'error': 'Failed to get transcript'}, status=500)
+
+    blog_content = generate_blog(transcript)
+
+    if not blog_content:
+      return JsonResponse({'error': 'Failed to generate blog article'}, status=500)
+  
+    # save blog article to database
+    new_blog_article = BlogPost.objects.create(
+        user=request.user,
+        youtube_title=title,
+        youtube_link=yt_link,
+        generated_content=blog_content,
+    )
+    new_blog_article.save()
+
+    return JsonResponse({'content': blog_content})
   else:
     return JsonResponse({'error': 'Invalid request method'}, status=405)
+
 
 # get the Youtube title  
 def yt_title(link):
   yt = YouTube(link)
   return yt.title
+
+
+def download_audio(link):
+  yt = YouTube(link)
+  video = yt.streams.filter(only_audio=True).first()
+  out_file = video.download(output_path=settings.MEDIA_ROOT)
+  base, ext = os.path.splitext(out_file)
+
+  new_file = base + '.mp3'
+  os.rename(out_file, new_file)
+
+  return new_file
+
+
+def get_transcription(link):
+  audio_file = download_audio(link)
+  aai.settings.api_key = os.environ.get("AAI_KEY")
+
+  transcriber = aai.Transcriber()
+  transcript = transcriber.transcribe(audio_file)
+
+  return transcript.text
+
+
+def generate_blog(transcript):
+  client = OpenAI(
+    api_key=os.environ.get("OPENAI_KEY"),
+   )
+
+  prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but don't make it look like a youtube video, make it look like a proper blog article: \n\n{transcript}\n\nArticle:"
+
+  response = client.completions.create(
+    model='gpt-3.5-turbo-instruct',
+    prompt=prompt,
+    max_tokens=1000
+  )
+
+  content = response.choices[0].text.strip()
+
+  return content
